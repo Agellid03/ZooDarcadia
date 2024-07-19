86521abdfcf1552a13105e89d432abb65b2b732c
O.B Sofiane <104198135+Agellid03@users.noreply.github.com>
2024-07-19 16:34:32 +0200

    Création de la bannière et ajout dans les pages principales + stylisation primaire

--- src/pages/Habitats.jsx
+++ src/pages/Habitats.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import BigTitle from "../components/BigTitle";
+import "../styles/bigTitle.css";
 const Habitats = () => {
-  return <p>Habitats</p>;
+  return (
+    <BigTitle
+      title="Bienvenue au Zoo D’Arcadia"
+      subtitle="Tous nos habitats !"
+      imageUrl="./images/Savane.jpeg"
+    />
+  );
 };
 
 export default Habitats;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import BigTitle from "../components/BigTitle";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <BigTitle
+        title="Bienvenue au Zoo D’Arcadia"
+        subtitle="Un lieu préservé pour s'emmerveiller!"
+        imageUrl="./images/Savane.jpeg"
+        showButton={true}
+      />
+    </div>
+  );
 };
 
 export default Home;
